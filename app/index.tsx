241ff80915aa07bb43172c75b9fab38abe151aa5
duggster <duggster@gmail.com>
2024-11-10 22:20:58 -0500

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect } from "react";
+import { Text, View, Button } from "react-native";
+import * as Calendar from "expo-calendar";
+
+export default function Index() {
+  const [status, requestPermission] = Calendar.useCalendarPermissions();
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Calendar.requestCalendarPermissionsAsync();
+      if (status === 'granted') {
+        const calendars = await Calendar.getCalendarsAsync(Calendar.EntityTypes.EVENT);
+        console.log('Here are all your calendars:');
+        console.log({ calendars });
+      }
+    })();
+  }, []);
+
+  const onPress = async () => {
+    const permissions = await Calendar.requestCalendarPermissionsAsync();
+    console.log(permissions);
+  }
+
+  return (
+    <View>
+      <Text>Calendar Permission Status: {status?.status}</Text>
+      <Button title="Request Permission" onPress={onPress} />
+    </View>
+  );
+}
